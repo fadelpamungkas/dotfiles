99b8aded7a08e7955657d871862b279a88255460
fadelpamungkas <fadelpamungkas3@gmail.com>
2023-03-27 21:52:27 +0700

    fix error comment nvim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -12,28 +12,30 @@ return {
 	dependencies = {
 		"JoosepAlviste/nvim-ts-context-commentstring",
 	},
-	opts = {
-		padding = true,
-		sticky = true,
-		ignore = nil,
-		toggler = {
-			line = "gcc",
-			block = "gbc",
-		},
-		opleader = {
-			line = "gc",
-			block = "gb",
-		},
-		extra = {
-			above = "gcO",
-			below = "gco",
-			eol = "gcA",
-		},
-		mappings = {
-			basic = true,
-			extra = true,
-		},
-		pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-		post_hook = nil,
-	},
+	opts = function()
+		require("Comment").setup({
+			padding = true,
+			sticky = true,
+			ignore = nil,
+			toggler = {
+				line = "gcc",
+				block = "gbc",
+			},
+			opleader = {
+				line = "gc",
+				block = "gb",
+			},
+			extra = {
+				above = "gcO",
+				below = "gco",
+				eol = "gcA",
+			},
+			mappings = {
+				basic = true,
+				extra = true,
+			},
+			pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
+			post_hook = nil,
+		})
+	end,
 }

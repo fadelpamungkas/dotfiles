b1ed730bad733bba788bb207fcce0c5ad0929693
fadelpamungkas <fadelpamungkas3@gmail.com>
2023-03-29 01:36:50 +0700

    update lazy plugin manager config

--- lua/lazyconfig.lua
+++ lua/lazyconfig.lua
@@ -16,8 +16,15 @@ require("lazy").setup("plugins", {
 	defaults = {
 		lazy = true, -- should plugins be lazy-loaded?
 		version = nil,
+		-- default `cond` you can use to globally disable a lot of plugins
+		-- when running inside vscode for example
+    -- @type boolean|fun(self:LazyPlugin):boolean|nil
+		cond = nil,
 		-- version = "*", -- enable this to try installing the latest stable versions of plugins
 	},
+	-- leave nil when passing the spec as the first argument to setup()
+  -- @type LazySpec
+	spec = nil,
 	lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json", -- lockfile generated after running update.
 	concurrency = nil, ---@type number limit the maximum amount of concurrent tasks
 	git = {
@@ -26,12 +33,17 @@ require("lazy").setup("plugins", {
 		log = { "--since=3 days ago" }, -- show commits from the last 3 days
 		timeout = 120, -- kill processes that take more than 2 minutes
 		url_format = "https://github.com/%s.git",
+		-- lazy.nvim requires git >=2.19.0. If you really want to use lazy with an older version,
+		-- then set the below to false. This should work, but is NOT supported and will
+		-- increase downloads a lot.
+		filter = true,
 	},
 	dev = {
 		-- directory where you store your local plugin projects
 		path = "~/projects",
 		---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
 		patterns = {}, -- For example {"folke"}
+		fallback = false, -- Fallback to git when local plugin doesn't exist
 	},
 	install = {
 		-- install missing plugins on startup. This doesn't increase startup time.
@@ -41,7 +53,8 @@ require("lazy").setup("plugins", {
 	},
 	ui = {
 		-- a number <1 is a percentage., >1 is a fixed size
-		size = { width = 0.8, height = 0.8 },
+		size = { width = 1, height = 1 },
+		wrap = true, -- wrap the lines in the ui
 		-- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
 		border = "none",
 		icons = {
@@ -50,14 +63,54 @@ require("lazy").setup("plugins", {
 			event = "",
 			ft = " ",
 			init = " ",
+			import = " ",
 			keys = " ",
+			lazy = "󰒲 ",
+			loaded = "●",
+			not_loaded = "○",
 			plugin = " ",
 			runtime = " ",
 			source = " ",
 			start = "",
 			task = "✔ ",
+			list = {
+				"●",
+				"➜",
+				"★",
+				"‒",
+			},
 		},
+		-- leave nil, to automatically select a browser depending on your OS.
+		-- If you want to use a specific browser, you can define it here
+		browser = nil, ---@type string?
 		throttle = 20, -- how frequently should the ui process render events
+		custom_keys = {
+			-- you can define custom key maps here.
+			-- To disable one of the defaults, set it to false
+
+			-- -- open lazygit log
+			-- ["<localleader>l"] = function(plugin)
+			-- 	require("lazy.util").float_term({ "lazygit", "log" }, {
+			-- 		cwd = plugin.dir,
+			-- 	})
+			-- end,
+			--
+			-- -- open a terminal for the plugin dir
+			-- ["<localleader>t"] = function(plugin)
+			-- 	require("lazy.util").float_term(nil, {
+			-- 		cwd = plugin.dir,
+			-- 	})
+			-- end,
+		},
+	},
+	diff = {
+		-- diff command <d> can be one of:
+		-- * browser: opens the github compare view. Note that this is always mapped to <K> as well,
+		--   so you can have a different command for diff <d>
+		-- * git: will run git diff and open a buffer with filetype git
+		-- * terminal_git: will open a pseudo terminal with git diff
+		-- * diffview.nvim: will open Diffview to show the diff
+		cmd = "git",
 	},
 	checker = {
 		-- automatically check for plugin updates
@@ -74,24 +127,17 @@ require("lazy").setup("plugins", {
 	performance = {
 		cache = {
 			enabled = true,
-			path = vim.fn.stdpath("state") .. "/lazy/cache",
-			-- Once one of the following events triggers, caching will be disabled.
-			-- To cache all modules, set this to `{}`, but that is not recommended.
-			-- The default is to disable on:
-			--  * VimEnter: not useful to cache anything else beyond startup
-			--  * BufReadPre: this will be triggered early when opening a file from the command line directly
-			disable_events = { "VimEnter", "BufReadPre" },
 		},
 		reset_packpath = true, -- reset the package path to improve startup time
 		rtp = {
 			reset = true, -- reset the runtime path to $VIMRUNTIME and your config directory
 			---@type string[]
-			paths = {}, -- add any custom paths here that you want to indluce in the rtp
+			paths = {}, -- add any custom paths here that you want to includes in the rtp
 			---@type string[] list any plugins you want to disable here
 			disabled_plugins = {
 				"gzip",
 				"matchit",
-				"matchparen",
+				-- "matchparen",
 				"netrwPlugin",
 				"tarPlugin",
 				"tohtml",
@@ -104,9 +150,11 @@ require("lazy").setup("plugins", {
 	-- so :help works even for plugins that don't have vim docs.
 	-- when the readme opens with :help it will be correctly displayed as markdown
 	readme = {
+		enabled = true,
 		root = vim.fn.stdpath("state") .. "/lazy/readme",
-		files = { "README.md" },
+		files = { "README.md", "lua/**/README.md" },
 		-- only generate markdown helptags for plugins that dont have docs
 		skip_if_doc_exists = true,
 	},
+	state = vim.fn.stdpath("state") .. "/lazy/state.json", -- state info for checker and other things
 })

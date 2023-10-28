59b104b9bd2c20f8e4f53cb9e139201f4245ed53
Fadel Pamungkas <fadelpamungkas3@gmail.com>
2023-10-28 16:31:18 +0700

    feat(cmd): add feature that compare clipboard with buffer or visual selection

--- lua/commands.lua
+++ lua/commands.lua
@@ -62,3 +62,49 @@ local function toggle_hlsearch(char)
 end
 
 vim.on_key(toggle_hlsearch, ns)
+
+-- Create a new scratch buffer
+vim.api.nvim_create_user_command("Ns", function()
+	vim.cmd([[
+		execute 'vsplit | enew'
+		setlocal buftype=nofile
+		setlocal bufhidden=hide
+		setlocal noswapfile
+	]])
+end, { nargs = 0 })
+
+-- Compare clipboard to current buffer
+vim.api.nvim_create_user_command("CompareClipboard", function()
+	local ftype = vim.api.nvim_eval("&filetype") -- original filetype
+	vim.cmd([[
+		tabnew %
+		Ns
+		normal! P
+		windo diffthis
+	]])
+	vim.cmd("set filetype=" .. ftype)
+end, { nargs = 0 })
+
+-- Assign it to a keymap
+vim.keymap.set("n", "<leader>vc", "<cmd>CompareClipboard<cr>")
+
+-- Compare clipboard to visual selection
+vim.api.nvim_create_user_command("CompareClipboardSelection", function()
+	vim.cmd([[
+		" yank visual selection to z register
+		normal! gv"zy
+		" open new tab, set options to prevent save prompt when closing
+		execute 'tabnew | setlocal buftype=nofile bufhidden=hide noswapfile'
+		" paste z register into new buffer
+		normal! V"zp
+		Ns
+		normal! Vp
+		windo diffthis
+	]])
+end, {
+	nargs = 0,
+	range = true,
+})
+
+-- Assign it to a keymap
+vim.keymap.set("v", "<leader>vc", "<esc><cmd>CompareClipboardSelection<cr>")
